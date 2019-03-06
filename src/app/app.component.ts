860a0713a92b450966614ad0ab0e27302dcf8b3a
MuhammadInaamMunir <munni7474@gmail.com>
2019-03-06 18:17:53 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import {Document, Paragraph, Packer} from 'docx';
+import * as FileSaver from 'file-saver';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularWordExample';
+  
+  public person = {
+    firstname:"",
+    lastname:""
+  }
+  submit(){
+    const document = new Document();
+    document.addParagraph(new Paragraph("First Name: "+ this.person.firstname));
+    document.addParagraph(new Paragraph("Last Name: "+ this.person.lastname));
+    const packer = new Packer();
+    packer.toBlob(document).then(blob => {
+      FileSaver.saveAs(blob,"test.docx");
+    });
+  }
 }
